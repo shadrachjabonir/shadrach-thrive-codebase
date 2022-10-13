e9431f1d36191c473cf0ed85e98cf9c8648a4d32
Shadrach Jabonir <shadrachjabonir@gmail.com>
2022-10-13 19:24:39 +0700

    berubah konfigurasi dan format api

--- src/main/java/com/thrive/GreetingResource.java
+++ src/main/java/com/thrive/GreetingResource.java
@@ -1,5 +1,8 @@
 package com.thrive;
 
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +11,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name ="greeting.message")
+    String myName;
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from "+myName;
     }
 }
